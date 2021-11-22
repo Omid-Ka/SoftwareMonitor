ebe584d9c378e4d5b4b46738f3ea722a3dfd1ea8
Omid <omid.kashki93@gmail.com>
2021-11-22 08:07:31 +0330

    14000901

---
+++ Domain/Models/Log/UserLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Models.Enum;
+
+namespace Domain.Models.Log
+{
+    public class UserLog:BaseEntity
+    {
+        public int EntityId { get; set; }
+        public string EntityType { get; set; }
+        public UserLogType Type { get; set; }
+    }
+}
